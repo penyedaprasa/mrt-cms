4b7e871d7acdc401e66c32834898b86edc7eb1c5
Sony Wibisono <wibisono.sony@gmail.com>
2020-02-04 11:36:00 +0700

    add API menu and banner

---
+++ app/Http/Controllers/API/BannerController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Banner;
+use Illuminate\Http\Request;
+
+class BannerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $banners = Banner::all();
+        $msg = array('status'=>true,'banners'=>$banners,'message'=>'Get All Banner');
+        return response()->json($msg);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Banner  $banner
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Banner $banner)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Banner  $banner
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Banner $banner)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Banner  $banner
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Banner $banner)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Banner  $banner
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Banner $banner)
+    {
+        //
+    }
+}
